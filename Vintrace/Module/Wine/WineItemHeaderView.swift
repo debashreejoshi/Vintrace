29ac7e847359eae2839d426e1890d2e831fe4103
debashreejoshi <djoshi31@gmail.com>
2023-07-02 23:28:39 +1000

    header view added

---
+++ Vintrace/Module/Wine/WineItemHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  WineItemHeaderView.swift
+//  Vintrace
+//
+//  Created by Debashree Joshi on 2/7/2023.
+//
+
+import UIKit
+
+class WineItemHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
